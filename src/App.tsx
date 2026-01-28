d1afb4b2162397dc85255f7fd1bb2acc5c9f6db6
Fababum <fabian.spiri@gmx.ch>
2026-01-28 13:38:11 +0100

    fix: design bisschen cleaner gestaltet

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import NavBar from "./components/navBar/navBar";
 import Home from "./components/home/home";
 import About from "./components/about/about";
@@ -8,17 +8,21 @@ import CalendarAI from "./components/CalendarAI/CalendarAI";
 import NotFound from "./components/error/NotFound";
 
 function App() {
+  const location = useLocation();
+
   return (
     <>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/chatBot" element={<ChatComponent />} />
-        <Route path="/calendarai" element={<CalendarAI />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <div className="pageTransition" key={location.pathname}>
+        <Routes location={location}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/chatBot" element={<ChatComponent />} />
+          <Route path="/calendarai" element={<CalendarAI />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
     </>
   );
 }
